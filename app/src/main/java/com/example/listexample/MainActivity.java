18fe6ac0f52f528aacc273270d98621f8438b4a4
CAT <cavilest@gmail.com>
2023-06-29 15:07:26 -0400

    Ejercicio indivdual 12. - Se agregan imágenes. - Se agrega listado de platos. - Se corrigen errores de diseño.

--- app/src/main/java/com/example/listexample/MainActivity.java
+++ app/src/main/java/com/example/listexample/MainActivity.java
@@ -9,20 +9,27 @@ import android.view.View;
 import com.example.listexample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Creación de clase Binding.
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Creación de una instancia de la clase Binding para ser usada en el layout.
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
+        // Llamado al método para generar la vista.
         setContentView(binding.getRoot());
 
+        // Enlace del botón del diseño mediante view binding.
         binding.menuButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+
+                // Creación de una instancia de la clase Intent de tipo explícito para enlazar ambas activities.
                 Intent explicitIntent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(explicitIntent);
             }
--- app/src/main/java/com/example/listexample/SecondActivity.java
+++ app/src/main/java/com/example/listexample/SecondActivity.java
@@ -11,21 +11,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
+
+// Creación de clase Binding.
 private ActivitySecondBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Creación de una instancia de la clase Binding para ser usada en el layout.
         binding = ActivitySecondBinding.inflate(getLayoutInflater());
 
+        // Llamado al método para generar la vista.
         setContentView(binding.getRoot());
 
+        // Se crea un listado para agregar platos a un menú
         List<String> plates = new ArrayList<String>();
 
-        plates.add("");
+        plates.add("Sanguche de pescado");
+        plates.add("Sopaipillas con pebre");
+        plates.add("Pan con papas fritas");
+        plates.add("Papapleto");
+        plates.add("Sanguche de potito");
+        plates.add("Choripan");
+        plates.add("Salchipapas");
+        plates.add("Sushipleto");
+        plates.add("Anticuchos");
+        plates.add("Papas fritas");
+        plates.add("Pizza del día");
+        plates.add("Sopaipleto");
+        plates.add("Perol");
+        plates.add("Empanada de queso");
+        plates.add("Cigarros sueltos");
+        plates.add("Galletas carioca");
+        plates.add("Melvin");
+        plates.add("Pipeño");
+        plates.add("Happy cola");
+        plates.add("Jugos naturales");
 
+        // Se crea una instancia de la clase ArrayAdapter para que adapte el listado creado.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, plates);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,plates);
+        // Mediante view binding se llama al método setAdapter para que produzca la vista del listado creado.
         binding.platesList.setAdapter(adapter);
     }
 }
