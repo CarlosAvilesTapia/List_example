dcf02f68566d562f3a07973aae10df83f38d702b
CAT <cavilest@gmail.com>
2023-06-28 22:46:19 -0400

    Ejercicio indivdual 12. - Listado enlazado.

--- app/src/main/java/com/example/listexample/SecondActivity.java
+++ app/src/main/java/com/example/listexample/SecondActivity.java
@@ -3,12 +3,29 @@ package com.example.listexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.example.listexample.databinding.ActivitySecondBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
-
+private ActivitySecondBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        List<String> plates = new ArrayList<String>();
+
+        plates.add("");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,plates);
+        binding.platesList.setAdapter(adapter);
     }
 }
